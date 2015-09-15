0462460f092f6e60ddc628c0d34edcc0ef17cb46
ml <ml@Macintosh.local>
2015-09-15 20:54:28 +0800

    invoker

---
+++ invoke.go
@@ -0,0 +1,219 @@
+package dingo
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+//
+// errors
+//
+
+type Invoker interface {
+	Invoke(f interface{}, param []interface{}) ([]interface{}, error)
+}
+
+type _invoker struct {
+}
+
+func (vk *_invoker) convert2slice(v, r reflect.Value, rt reflect.Type) (err error) {
+	if v.Kind() != reflect.Slice {
+		err = errors.New(fmt.Sprintf("Only Slice not %v convertible to slice", v.Kind().String()))
+		return
+	}
+
+	r.Set(reflect.MakeSlice(rt, 0, v.Len()))
+	for i := 0; i < v.Len(); i++ {
+		e := v.Index(i)
+		if !e.CanInterface() {
+			continue
+		}
+		converted, err_ := vk.convert(e.Interface(), rt.Elem())
+		if err_ != nil {
+			err = err_
+			break
+		} else {
+			r.Set(reflect.Append(r, converted))
+		}
+	}
+
+	return
+}
+
+func (vk *_invoker) convert2map(v, r reflect.Value, rt reflect.Type) (err error) {
+	if v.Kind() != reflect.Map {
+		err = errors.New(fmt.Sprintf("Only Map not %v convertible to map", v.Kind().String()))
+		return
+	}
+
+	// init map
+	r.Set(reflect.MakeMap(rt))
+
+	keys := v.MapKeys()
+	for _, k := range keys {
+		e := v.MapIndex(k)
+		if !e.CanInterface() {
+			continue
+		}
+		converted, err_ := vk.convert(e.Interface(), rt.Elem())
+		if err_ != nil {
+			// propagate error
+			err = err_
+			break
+		} else {
+			r.SetMapIndex(k, converted)
+		}
+	}
+
+	return
+}
+func (vk *_invoker) convert2struct(val interface{}, v, r reflect.Value, rt reflect.Type) (err error) {
+	if v.Kind() != reflect.Map {
+		err = errors.New(fmt.Sprintf("Only Map not %v convertible to struct", v.Kind().String()))
+		return
+	}
+	for i := 0; i < r.NumField(); i++ {
+		fv := r.Field(i)
+		if !fv.CanSet() {
+			continue
+		}
+
+		var converted reflect.Value
+
+		ft := rt.Field(i)
+		if ft.Anonymous {
+			converted, err = vk.convert(val, ft.Type)
+		} else {
+			// json tags
+			// TODO: move this to a private function
+			key := ft.Tag.Get("json")
+			if key != "" {
+				key = strings.Trim(key, "\"")
+				keys := strings.Split(key, ",")
+				key = keys[0]
+				if key == "-" {
+					key = ""
+				}
+			}
+			if key == "" {
+				key = ft.Name
+			}
+			mv := v.MapIndex(reflect.ValueOf(key))
+			if !mv.IsValid() {
+				err = errors.New(fmt.Sprintf("Invalid value returned from map by key: %v", ft))
+				break
+			}
+
+			if !mv.CanInterface() {
+				continue
+			}
+			converted, err = vk.convert(mv.Interface(), fv.Type())
+		}
+
+		if err != nil {
+			break
+		}
+
+		fv.Set(converted)
+	}
+
+	return err
+}
+
+//
+// helper function for converting a value based on a type
+//
+func (vk *_invoker) convert(val interface{}, t reflect.Type) (reflect.Value, error) {
+	var err error
+
+	if val == nil {
+		if t.Kind() != reflect.Ptr {
+			err = errors.New("Can't pass nil for non-ptr parameter")
+		}
+		// for pointer type, reflect.Zero create a nil pointer
+		return reflect.Zero(t), err
+	}
+
+	v := reflect.ValueOf(val)
+	if v.Type().ConvertibleTo(t) {
+		return v.Convert(t), nil
+	}
+
+	deref := 0
+	// only reflect.Value from reflect.New is settable
+	// reflect.Zero is not.
+	ret := reflect.New(t)
+	for ; t.Kind() == reflect.Ptr; deref++ {
+		t = t.Elem()
+		ret.Elem().Set(reflect.New(t))
+		ret = ret.Elem()
+	}
+	elm := ret.Elem()
+
+	switch elm.Kind() {
+	case reflect.Struct:
+		err = vk.convert2struct(val, v, elm, t)
+	case reflect.Map:
+		err = vk.convert2map(v, elm, t)
+	case reflect.Slice:
+		err = vk.convert2slice(v, elm, t)
+	default:
+		_ = "breakpoint"
+	}
+
+	if deref == 0 {
+		ret = ret.Elem()
+	} else {
+		// derefencing to correct type
+		for deref--; deref > 0; deref-- {
+			ret = ret.Addr()
+		}
+	}
+
+	return ret, nil
+}
+
+//
+// reference implementation
+//	  https://github.com/codegangsta/inject/blob/master/inject.go
+//
+func (vk *_invoker) Invoke(f interface{}, param []interface{}) ([]interface{}, error) {
+	var err error
+
+	funcT := reflect.TypeOf(f)
+
+	// make sure parameter matched
+	if len(param) != funcT.NumIn() {
+		return nil, errors.New(fmt.Sprintf("Parameter Count mismatch: %v %v", len(param), funcT.NumIn()))
+	}
+
+	// convert param into []reflect.Value
+	var in = make([]reflect.Value, funcT.NumIn())
+	for i := 0; i < funcT.NumIn(); i++ {
+		in[i], err = vk.convert(param[i], funcT.In(i))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// invoke the function
+	ret := reflect.ValueOf(f).Call(in)
+	out := make([]interface{}, funcT.NumOut())
+
+	// we don't have to check the count of return values and out,
+	// if not match, it won't build
+
+	for i := 0; i < funcT.NumOut(); i++ {
+		if ret[i].CanInterface() {
+			out[i] = ret[i].Interface()
+		}
+	}
+
+	return out, nil
+}
+
+func NewInvoker() Invoker {
+	return &_invoker{}
+}
---
+++ invoke_test.go
@@ -0,0 +1,493 @@
+package dingo
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// simulate users passing a list of parameters,
+// marshaling/unmarshaling by JSON, finally becoming
+// a list of interface{}
+func ioJSON(v ...interface{}) ([]interface{}, error) {
+	var ret []interface{}
+
+	b, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(b, &ret)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+//
+// struct
+//
+type _struct struct {
+	Name  string
+	Count int
+}
+
+// struct with private field
+type _struct_private struct {
+	_struct
+	private int
+}
+
+//
+// embed struct
+//
+type _embed struct {
+	_struct
+	Age  int
+	Addr string
+}
+
+type _embed_with_collision struct {
+	_struct
+	Age  int
+	Addr string
+
+	// collision, named with tag
+	Name  string `json:"out.Name"`
+	Count int    `json:"out.Count,omitempty"`
+
+	// struct pointer
+	S *_struct
+}
+
+//
+// testing suite
+//
+type InvokeTestSuite struct {
+	suite.Suite
+
+	ivk Invoker
+}
+
+func (s *InvokeTestSuite) SetupSuite() {
+	// prepare invoker
+	s.ivk = NewInvoker()
+
+	// TODO: map
+	// TODO: slice
+}
+
+func TestInvokeTestSuiteMain(t *testing.T) {
+	suite.Run(t, &InvokeTestSuite{})
+}
+
+//
+// test cases
+//
+
+func (s *InvokeTestSuite) TestFloat64() {
+	// float64
+	{
+		called := float64(0.0)
+		chk := func(v float64) (float64, error) {
+			called = v
+			return v, nil
+		}
+
+		param, err := ioJSON(float64(1.0))
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(float64)
+				s.True(ok)
+				if ok {
+					s.Equal(float64(1.0), f)
+				}
+			}
+
+			s.Equal(float64(1.0), called)
+		}
+	}
+
+	// TODO: *float64
+	{
+	}
+}
+
+func (s *InvokeTestSuite) TestInt64() {
+	// int64
+	{
+		called := int64(0)
+		chk := func(v int64) (int64, error) {
+			called = v
+			return v, nil
+		}
+		param, err := ioJSON(int64(1))
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(int64)
+				s.True(ok)
+				if ok {
+					s.Equal(int64(1), f)
+				}
+			}
+
+			s.Equal(int64(1), called)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestString() {
+	{
+		called := ""
+		chk := func(v string) (string, error) {
+			called = v
+			return v, nil
+		}
+		param, err := ioJSON("test string")
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(string)
+				s.True(ok)
+				if ok {
+					s.Equal("test string", f)
+				}
+			}
+
+			s.Equal("test string", called)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestStruct() {
+	// *_struct
+	{
+		called := (*_struct)(nil)
+		chk := func(v *_struct) (*_struct, error) {
+			called = v
+			return v, nil
+		}
+		v := &_struct{Name: "Bob", Count: 10}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(*_struct)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(called, v)
+		}
+	}
+
+	// **_struct
+	{
+		called := (**_struct)(nil)
+		name := ""
+		count := 0
+		chk := func(v **_struct) (**_struct, error) {
+			called = v
+			if v != nil && *v != nil {
+				name = (**v).Name
+				count = (**v).Count
+			}
+			// access its element
+			return v, nil
+		}
+		{
+			v_ := &_struct{Name: "Bob", Count: 10}
+			// pointer-2-pointer
+			v := &v_
+			param, err := ioJSON(v)
+			s.Nil(err)
+			s.NotNil(param)
+			if param != nil {
+				ret, err := s.ivk.Invoke(chk, param)
+				s.Nil(err)
+				s.Len(ret, 2)
+				if ret != nil && len(ret) > 0 {
+					f, ok := ret[0].(**_struct)
+					s.True(ok)
+					if ok {
+						s.Equal(v, f)
+					}
+				}
+
+				s.Equal(v, called)
+				s.Equal("Bob", name)
+				s.Equal(10, count)
+			}
+		}
+
+		// calling with nil
+		{
+			param, err := ioJSON(nil)
+			s.Nil(err)
+			s.NotNil(param)
+			if param != nil {
+				ret, err := s.ivk.Invoke(chk, param)
+				s.Nil(err)
+				s.Len(ret, 2)
+				if ret != nil && len(ret) > 0 {
+					f, ok := ret[0].(**_struct)
+					s.True(ok)
+					if ok {
+						s.Equal((**_struct)(nil), f)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestEmbed() {
+	// *_embed
+	{
+		called := (*_embed)(nil)
+		age, count, name, address := 0, 0, "", ""
+		chk := func(v *_embed) (*_embed, error) {
+			called = v
+			if v != nil {
+				name = v.Name
+				age = v.Age
+				count = v.Count
+				address = v.Addr
+			}
+			return v, nil
+		}
+		v := &_embed{_struct: _struct{Name: "Bob", Count: 10}, Age: 100, Addr: "heaven"}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(*_embed)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+			s.Equal(100, age)
+			s.Equal(10, count)
+			s.Equal("Bob", name)
+			s.Equal("heaven", address)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestEmbedWithCollision() {
+	// *_embed_with_collision
+	{
+		called := (*_embed_with_collision)(nil)
+		age, count, name, address, count_, name_, count_s, name_s := 0, 0, "", "", 0, "", 0, ""
+		chk := func(v *_embed_with_collision) (*_embed_with_collision, error) {
+			called = v
+			if v != nil {
+				name = v.Name
+				age = v.Age
+				count = v.Count
+				address = v.Addr
+				name_ = v._struct.Name
+				count_ = v._struct.Count
+				name_s = v.S.Name
+				count_s = v.S.Count
+			}
+			return v, nil
+		}
+		v := &_embed_with_collision{
+			_struct: _struct{Name: "Bob", Count: 10},
+			Age:     100,
+			Addr:    "heaven",
+			Name:    "Mary",
+			Count:   11,
+			S:       &_struct{Name: "Tom", Count: 12},
+		}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(*_embed_with_collision)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+			s.Equal(100, age)
+			s.Equal(10, count_)
+			s.Equal("Bob", name_)
+			s.Equal("heaven", address)
+			s.Equal("Mary", name)
+			s.Equal(11, count)
+			s.Equal("Tom", name_s)
+			s.Equal(12, count_s)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestMap() {
+	// map[string]int
+	{
+		var called map[string]int
+		chk := func(v map[string]int) (map[string]int, error) {
+			called = v
+			return v, nil
+		}
+		v := map[string]int{"Tom": 12, "Bob": 10, "Mary": 11}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(map[string]int)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+		}
+	}
+
+	// map[string]*_struct
+	{
+		var called map[string]*_struct
+		name, count := "", 0
+		chk := func(v map[string]*_struct) (map[string]*_struct, error) {
+			called = v
+			name = v["a"].Name
+			count = v["a"].Count
+			s.Nil(v["d"])
+			return v, nil
+		}
+		v := map[string]*_struct{
+			"a": &_struct{Name: "Mary", Count: 11},
+			"b": &_struct{Name: "Bob", Count: 10},
+			"c": &_struct{Name: "Tom", Count: 12},
+			"d": nil,
+		}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].(map[string]*_struct)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+			s.Equal(v["a"].Name, name)
+			s.Equal(v["a"].Count, count)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestSlice() {
+	// []string
+	{
+		var called []string
+		chk := func(v []string) ([]string, error) {
+			called = v
+			return v, nil
+		}
+		v := []string{"Tom", "Bob", "Mary"}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].([]string)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+		}
+	}
+
+	// TODO: []*_struct
+	{
+		var called []*_struct
+		chk := func(v []*_struct) ([]*_struct, error) {
+			called = v
+			s.Len(v, 4)
+			s.Nil(v[3])
+			return v, nil
+		}
+		v := []*_struct{
+			&_struct{Name: "Mary", Count: 11},
+			&_struct{Name: "Bob", Count: 10},
+			&_struct{Name: "Tom", Count: 12},
+			nil,
+		}
+		param, err := ioJSON(v)
+		s.Nil(err)
+		s.NotNil(param)
+		if param != nil {
+			ret, err := s.ivk.Invoke(chk, param)
+			s.Nil(err)
+			s.Len(ret, 2)
+			if ret != nil && len(ret) > 0 {
+				f, ok := ret[0].([]*_struct)
+				s.True(ok)
+				if ok {
+					s.Equal(v, f)
+				}
+			}
+
+			s.Equal(v, called)
+		}
+	}
+}
+
+func (s *InvokeTestSuite) TestPrivateField() {
+}
